1ba1fe51d2945e0ce7e57b7f9f789bd2c6341959
BobrikovAleksey <bobrikov.spb@ya.ru>
2021-01-27 13:05:16 +0300

    Add phone menu   (#65)

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,5 @@
 import './modules/jquery.js';
-import './modules/slick.js';
-
-
-$('.feedback__reviews').slick({
-    infinite: true,
-    variableWidth: true,
-});
-
-$('.guarantee__content').slick({
-    infinite: true,
-    variableWidth: true,
-    arrows:	false,
-    draggable: false,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows:	true,
-                draggable: true,
-            },
-        },
-    ],
-});
-
-$('.partners__content').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-        {
-            breakpoint: 631,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            },
-        },
-    ],
-});
+import './slick-init.js';
 
 
 if (location.host === 'bobrikovaleksey.github.io') {
@@ -45,5 +8,66 @@ if (location.host === 'bobrikovaleksey.github.io') {
         if (href[0] === '/') {
             el.setAttribute('href', `/SendIt_Geekbrains${href}`);
         }
+        console.log(el.getAttribute('href'));
     });
 }
+
+
+const app = {
+    cache: {},
+    state: {},
+
+    /**
+     * Открывает всплывающее окно
+     * @param name String
+     */
+    openPopup(name) {
+        const $$ = this.cache;
+
+        if (!$$.initialized || !(name in $$.popup)) return;
+
+        !$$.main.hasAttribute('data-blur') && $$.main.setAttribute('data-blur', '');
+        $$.popup[name].hasAttribute('data-hide') && $$.popup[name].removeAttribute('data-hide');
+    },
+
+    /**
+     * Закрывает всплывающее окно / все всплывающие окна
+     * @param name String
+     */
+    closePopup(name = '') {
+        const $$ = this.cache;
+
+        if (!$$.initialized || (name !== '' && !(name in $$.popup))) return;
+
+        $$.main.hasAttribute('data-blur') && $$.main.removeAttribute('data-blur');
+        if (name === '') {
+            Object.keys($$.popup).forEach((name) => {
+                !$$.popup[name].hasAttribute('data-hide') && $$.popup[name].setAttribute('data-hide', '');
+            });
+        } else {
+            !$$.popup[name].hasAttribute('data-hide') && $$.popup[name].setAttribute('data-hide', '');
+        }
+    },
+
+    init() {
+        this.cache = {
+            initialized: true,
+            main: document.querySelector('main'),
+            popup: {
+                phoneMenu: document.querySelector('.phone-menu'),
+                contactForm: document.querySelector('.contact-form'),
+            },
+        };
+
+        window.addEventListener('resize', this.closePopup.bind(this, ''));
+        document.querySelectorAll('button[data-phone-menu="open"]').forEach((el) => {
+            el.addEventListener('click', this.openPopup.bind(this, "phoneMenu"));
+        });
+        document.querySelectorAll('button[data-phone-menu="close"]').forEach((el) => {
+            el.addEventListener('click', this.closePopup.bind(this, "phoneMenu"));
+        });
+    },
+};
+
+
+app.init();
---
+++ scripts/slick-init.js
@@ -0,0 +1,38 @@
+import './modules/slick.js';
+
+
+$('.feedback__reviews').slick({
+    infinite: true,
+    variableWidth: true,
+});
+
+$('.guarantee__content').slick({
+    infinite: true,
+    variableWidth: true,
+    arrows:	false,
+    draggable: false,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows:	true,
+                draggable: true,
+            },
+        },
+    ],
+});
+
+$('.partners__content').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 631,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            },
+        },
+    ],
+});
